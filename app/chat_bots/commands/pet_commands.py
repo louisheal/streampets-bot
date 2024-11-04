61f8e530c698125afec481a122f16eb3327a2c8e
louisheal <louisheal@live.co.uk>
2024-11-04 16:32:53 +0000

    created dynamic event names

--- app/chat_bots/commands/pet_commands.py
+++ app/chat_bots/commands/pet_commands.py
@@ -10,7 +10,7 @@ def setup_pet_commands(bot):
 
   @commands.command(name='jump')
   async def command_jump(ctx):
-    bot.announcer.announce(msg=ctx.author.name, event=JUMP)
+    bot.announcer.announce(msg=ctx.author.name, event=f'JUMP-{ctx.author.name}')
 
   @commands.command(name='color')
   async def command_color(ctx, color):
@@ -19,7 +19,7 @@ def setup_pet_commands(bot):
       return
     
     trex = bot.db.set_trex_color(ctx.author.name, Color.str_to_color(color))
-    bot.announcer.announce(msg=json.dumps(trex.to_dict()), event=COLOR)
+    bot.announcer.announce(msg=json.dumps(trex.to_dict()), event=f'COLOR-{ctx.author.name}')
 
   bot.add_command(command_jump)
   bot.add_command(command_color)
